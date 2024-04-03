12089c50d64c74984bb3efa7da9ad1848cfa9c7f
locnht-it <locnht.it@gmail.com>
2024-04-03 08:41:20 +0700

    first commit

---
+++ src/main/java/com/locnht/SpringSecurityWithJWTToken/SpringSecurityWithJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.locnht.SpringSecurityWithJWTToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locnht/SpringSecurityWithJWTToken/SpringSecurityWithJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locnht.SpringSecurityWithJWTToken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
